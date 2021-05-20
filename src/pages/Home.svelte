6ed079a51de09f3cd113ad8edfa603634140af0d
Ronald Arias <ariasron@hotmail.com>
2021-05-19 21:35:25 -0500

    Creates nav using Route, Router and Link from svelte-routing and displays pages accordingly

---
+++ src/pages/Home.svelte
@@ -0,0 +1,8 @@
+<Hero>
+	<a href="/products" use:link class="btn btn-primary btn-hero">shop now</a>
+</Hero>
+
+<script>
+	import Hero from '../components/Hero.svelte';
+	import { link } from 'svelte-routing';
+</script>
