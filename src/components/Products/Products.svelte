e840aee5eeab8e6e3c9bf866fe524cdb1a4c302a
Ronald Arias <ariasron@hotmail.com>
2021-05-21 10:20:42 -0500

    Adds products component and its store, also adds a function for flattenProducts

---
+++ src/components/Products/Products.svelte
@@ -0,0 +1,17 @@
+<h1>hello from products component</h1>
+{#each localProducts as product}
+	<h2>{product.title}</h2>
+{/each}
+
+<script>
+	import { onMount } from 'svelte';
+	import products from '../../stores/defaultProducts';
+
+	let localProducts = [];
+
+	onMount(() => {
+		products.subscribe((value) => {
+			localProducts = value;
+		});
+	});
+</script>
--- src/pages/Products.svelte
+++ src/pages/Products.svelte
@@ -1,1 +1,6 @@
 <h1>welcome to products page</h1>
+<Products />
+
+<script>
+	import Products from '../components/Products/Products.svelte';
+</script>
