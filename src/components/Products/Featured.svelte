adbb235d4af66f9aff8ffd44d1366067f1106b49
Ronald Arias <ariasron@hotmail.com>
2021-05-21 15:25:59 -0500

    Creates Featured component and displaying it on home page

---
+++ src/components/Products/Featured.svelte
@@ -0,0 +1,27 @@
+<!-- using $ on products will subscribe and unsubscribe when component is loaded and destroyed -->
+<!-- with $ we do not have access to set, update and subscribe methods, but we have the objects on products -->
+
+{#if featured.length}
+	<section class="section">
+		<h2 class="section-title">{title}</h2>
+		<div class="products-center">
+			{#each featured as product (product.id)}
+				<Product {product} />
+			{/each}
+		</div>
+	</section>
+{:else}
+	<Loading />
+{/if}
+
+<script>
+	import products from '../../stores/defaultProducts';
+	import Product from './Product.svelte';
+	import Loading from '../Loading.svelte';
+
+	$: featured = $products.filter((item) => item.featured === true);
+
+	let title = '';
+
+	export { title };
+</script>
