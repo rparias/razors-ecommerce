066f2116e53fe8b05f9976a953b891b6b3f9a2ec
Ronald Arias <ariasron@hotmail.com>
2021-05-22 14:48:01 -0500

    Adds a new global store to handle sidebar, cart and alert

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,8 @@
 <Router>
 	<Navbar />
-	<Sidebar />
+	{#if globalStore.sidebar}
+		<Sidebar />
+	{/if}
 	<Route path="/" component={Home} />
 	<Route path="/about" component={About} />
 	<Route path="/login" component={Login} />
@@ -25,4 +27,6 @@
 	// components
 	import Navbar from './components/Navbar/Navbar.svelte';
 	import Sidebar from './components/Navbar/Sidebar.svelte';
+	// global store
+	import globalStore from './stores/globalStore';
 </script>
