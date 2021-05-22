5b5f5fb910b018ef56e038ee463d9beea5615753
Ronald Arias <ariasron@hotmail.com>
2021-05-22 13:25:59 -0500

    Displays a single product on productTemplate page

--- src/pages/ProductTemplate.svelte
+++ src/pages/ProductTemplate.svelte
@@ -1,7 +1,35 @@
-<h1>welcome to product template page with id: {id}</h1>
+{#if !product}
+	<Loading />
+{:else}
+	<section class="single-product">
+		<a href="/products" use:link class="btn btn-primary">back to products</a>
+		<div class="single-product-container">
+			<article class="single-product-image">
+				<img src={product.image} alt={product.title} />
+			</article>
+			<article>
+				<h1>{product.title}</h1>
+				<h2>${product.price}</h2>
+				<p>{product.description}</p>
+				<button
+					class="btn btn-primary btn-block"
+					on:click={() => {
+						console.log('clicked add to cart');
+					}}>add to cart</button
+				>
+			</article>
+		</div>
+	</section>
+{/if}
 
 <script>
+	import { link } from 'svelte-routing';
+	import Loading from '../components/Loading.svelte';
+	import products from '../stores/defaultProducts';
+
 	let id;
+
+	$: product = $products.find((item) => item.id === parseInt(id));
 
 	export { id };
 </script>
