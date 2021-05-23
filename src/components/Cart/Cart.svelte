0bfe52cf74941f4af9d8f4a7770040daefd5265e
Ronald Arias <ariasron@hotmail.com>
2021-05-22 19:35:55 -0500

    Working on cart aside structure

--- src/components/Cart/Cart.svelte
+++ src/components/Cart/Cart.svelte
@@ -1,7 +1,47 @@
-<h2>cart</h2>
-<h2>cart</h2>
-<h2>cart</h2>
-<h2>cart</h2>
-<h2>cart</h2>
-<h2>cart</h2>
-<h2>cart</h2>
+<div class="cart-overlay" transition:blur>
+	<div class="cart-container" transition:fly={{ x: 100 }}>
+		<div class="cart" transition:fade={{ delay: 200 }}>
+			<div class="cart-header">
+				<button
+					class="btn-close"
+					on:click={() => {
+						globalStore.toggleItem('cart', false);
+					}}><i class="fas fa-window-close" /></button
+				>
+				<h2 class="cart-title">your bag</h2>
+				<span />
+			</div>
+			<p>cart items</p>
+			<div class="cart-footer">
+				{#if user}
+					<a
+						href="/checkout"
+						use:link
+						class="btn btn-primary btn-block"
+						on:click={() => {
+							globalStore.toggleItem('cart', false);
+						}}>checkout</a
+					>
+				{:else}
+					<p class="cart-login">
+						in order to checkout please <a
+							href="/login"
+							use:link
+							on:click={() => {
+								globalStore.toggleItem('cart', false);
+							}}>login</a
+						>
+					</p>
+				{/if}
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+	import globalStore from '../../stores/globalStore';
+	import { fly, fade, blur } from 'svelte/transition';
+	import { link } from 'svelte-routing';
+
+	let user = false;
+</script>
