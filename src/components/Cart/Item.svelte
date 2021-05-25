faf8ac934e8849adc8bb2485abc1f0e488f7593f
Ronald Arias <ariasron@hotmail.com>
2021-05-25 10:44:45 -0500

    Completes design for single cart item

--- src/components/Cart/Item.svelte
+++ src/components/Cart/Item.svelte
@@ -1,1 +1,42 @@
-<h1>single item cart</h1>
+<div class="cart-item">
+	<img src={image} alt={title} />
+	<div>
+		<h4>{title}</h4>
+		<h5>${price}</h5>
+		<button
+			class="cart-btn remove-btn"
+			on:click={() => {
+				console.log('remove item');
+			}}>remove</button
+		>
+	</div>
+	<div>
+		<button
+			class="cart-btn amount-btn"
+			on:click={() => {
+				console.log('increase item');
+			}}
+		>
+			<i class="fas fa-chevron-up" />
+		</button>
+		<p class="item-amount">{amount}</p>
+		<button
+			class="cart-btn amount-btn"
+			on:click={() => {
+				console.log('decrease item');
+			}}
+		>
+			<i class="fas fa-chevron-down" />
+		</button>
+	</div>
+</div>
+
+<script>
+	let amount;
+	let id;
+	let image;
+	let price;
+	let title;
+
+	export { amount, id, image, price, title };
+</script>
