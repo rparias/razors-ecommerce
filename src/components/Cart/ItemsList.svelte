ea7dd65c95b3ec176460061ba31ebcecd93780a4
Ronald Arias <ariasron@hotmail.com>
2021-05-25 10:50:03 -0500

    Adds animations for each item on cart

--- src/components/Cart/ItemsList.svelte
+++ src/components/Cart/ItemsList.svelte
@@ -1,7 +1,9 @@
 <section class="cart-items">
 	<article>
 		{#each $cartStore as cartItem, index (cartItem.id)}
-			<Item {...cartItem} />
+			<div in:fly={{ delay: (index + 1) * 500, x: 100 }} on:fly={{ x: -100 }} animate:flip>
+				<Item {...cartItem} />
+			</div>
 		{:else}
 			<h2 class="empty-cart">is currently empty</h2>
 		{/each}
@@ -9,6 +11,8 @@
 </section>
 
 <script>
+	import { fly } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
 	import Item from './Item.svelte';
 	import cartStore from '../../stores/cart';
 </script>
