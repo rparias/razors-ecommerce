f6874f33b856cfbf5fa74c8627045bb1e53e26fb
Ronald Arias <ariasron@hotmail.com>
2021-05-28 08:40:10 -0500

    Updates user store

--- src/stores/storeUser.js
+++ src/stores/storeUser.js
@@ -1,5 +1,19 @@
 import { writable } from 'svelte/store';
 
-const storeUser = writable({ username: null, jwt: null });
+const storeUser = writable(getStorageUser());
+
+function getStorageUser() {
+	return localStorage.getItem('user')
+		? JSON.parse(localStorage.getItem('user'))
+		: { username: null, jwt: null };
+}
+
+export function setStorageUser(user) {
+	localStorage.setItem('user', JSON.stringify(user));
+}
+
+export function setUser(user) {
+	storeUser.set(user);
+}
 
 export default storeUser;
