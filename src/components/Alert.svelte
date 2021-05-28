ec8832ebf506c423a25125a55febd0b491f5016c
Ronald Arias <ariasron@hotmail.com>
2021-05-28 11:52:02 -0500

    Creates an alert component that will be handled with globalStore.alert

---
+++ src/components/Alert.svelte
@@ -0,0 +1,14 @@
+<div class="alert-container" in:fly={{ y: -200, duration: 1000 }} out:fade={{ duration: 0 }}>
+	<div class="alert">
+		<p>some dummy text</p>
+		<button on:click={handleClose} class="alert-close"><i class="fas fa-window-close" /></button>
+	</div>
+</div>
+
+<script>
+	import { fly, fade } from 'svelte/transition';
+
+	const handleClose = () => {
+		console.log('alert closed');
+	};
+</script>
