a5ee7ac249a8df20498829117ce04471bffdcf49
Ronald Arias <ariasron@hotmail.com>
2021-05-28 21:36:48 -0500

    Adds stripe html elements on checkout page with info

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -7,6 +7,21 @@
 				<label for="name">your name</label>
 				<input type="text" id="name" bind:value={name} />
 				<!-- stripe stuff -->
+				<div class="stripe-input">
+					<label for="card-element">Credit or Debit Card</label>
+					<p class="stripe-info">
+						Test using this credit card:
+						<span>4242 4242 4242 4242</span>
+						<br />
+						enter any 5 digits for the zip code
+						<br />
+						enter any 3 digits for the CVC
+					</p>
+					<div id="card-element" bind:this={cardElement}>
+						<!-- stripe -->
+					</div>
+					<div id="card-errors" bind:this={cardErrors} role="alert" />
+				</div>
 				<!-- end of stripe stuff -->
 				{#if isEmpty}
 					<p class="form-empty">please fill out name field</p>
@@ -34,6 +49,13 @@
 	import storeUser from '../stores/storeUser';
 
 	let name = '';
+
+	// stripe vars
+	// let card;
+	let cardElement;
+	let cardErrors;
+	// let elements;
+	// let stripe;
 
 	$: isEmpty = !name;
 
