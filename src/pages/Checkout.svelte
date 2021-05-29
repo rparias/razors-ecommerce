bf5348465f531c3112f995b98bc301756e1fa9f3
Ronald Arias <ariasron@hotmail.com>
2021-05-28 21:13:45 -0500

    Creates checkout form basic setup

--- src/pages/Checkout.svelte
+++ src/pages/Checkout.svelte
@@ -1,5 +1,25 @@
 {#if $storeCartTotal > 0}
-	<h1>form</h1>
+	<section class="form">
+		<h2 class="section-title">checkout</h2>
+		<form class="checkout-form" on:submit|preventDefault={handleSubmit}>
+			<h3>order total: ${$storeCartTotal}</h3>
+			<div class="form-control">
+				<label for="name">your name</label>
+				<input type="text" id="name" bind:value={name} />
+				<!-- stripe stuff -->
+				<!-- end of stripe stuff -->
+				{#if isEmpty}
+					<p class="form-empty">please fill out name field</p>
+				{/if}
+				<button
+					type="submit"
+					class="btn btn-block btn-primary"
+					disabled={isEmpty}
+					class:disabled={isEmpty}>submit</button
+				>
+			</div>
+		</form>
+	</section>
 {:else}
 	<div class="checkout-empty">
 		<h2>your cart is empty</h2>
@@ -13,9 +33,17 @@
 	import { storeCartTotal } from '../stores/cart';
 	import storeUser from '../stores/storeUser';
 
+	let name = '';
+
+	$: isEmpty = !name;
+
 	onMount(() => {
 		if (!$storeUser.jwt) {
 			navigate('/');
 		}
 	});
+
+	function handleSubmit() {
+		console.log('form submitted');
+	}
 </script>
